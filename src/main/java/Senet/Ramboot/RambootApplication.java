3a04e70a91a865c95fc54151b31a001a40535db2
Marcos <marcoscurro1234@gmail.com>
2025-03-10 19:19:09 +0100

    first commit

---
+++ src/main/java/Senet/Ramboot/RambootApplication.java
@@ -0,0 +1,13 @@
+package Senet.Ramboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RambootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RambootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Senet/Ramboot/RambootApplicationTests.java
@@ -0,0 +1,13 @@
+package Senet.Ramboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RambootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
